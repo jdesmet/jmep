f07632ed2982b5e882e78f1e1870d213f1d87a14
yo.desmet@gmail.com <yo.desmet@gmail.com@281e06b9-c490-646c-95d8-8800e615b14b>
2014-01-20 23:14:22 +0000

    Rename

---
+++ src/com/googlecode/jmep/hooks/Constant.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.googlecode.jmep.hooks;
+
+/**
+ *
+ * @author jd3714
+ */
+final public class Constant implements Variable {
+  final private Object value;
+
+  public Constant(Number value) {
+    this.value = value;
+  }
+
+  public Constant(String value) {
+    this.value = value;
+  }
+
+  public Object getValue() {
+    return this.value;
+  }
+
+}
